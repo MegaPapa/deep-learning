6e4b83ccd7313d685854d8a388defba53cbb90fe
Maxim Petrusevich <maxim.petrusevich@minsk.leveris.com>
2020-02-14 11:47:44 +0300

    Divided some consts to external file

---
+++ dl_1/const.py
@@ -0,0 +1,14 @@
+FIRST_DATASET_URL = "https://commondatastorage.googleapis.com/books1000/notMNIST_large.tar.gz"
+FIRST_UNIQ_DATASET_PATH_NAME = "notMnist_large"
+
+SECOND_DATASET_URL = "https://commondatastorage.googleapis.com/books1000/notMNIST_small.tar.gz"
+SECOND_UNIQ_DATASET_PATH_NAME = "notMnist_small"
+
+# Constant to check classes balancing (for small dataset)
+CLASSES_DIFFERENCE_ERROR = 25
+
+LEARNING_LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+TRAIN_SET_PERCENTS = 60.0
+VALIDATION_SET_PERCENTS = 20.0
+TEST_SET_PERCENTS = 20.0
--- dl_1/lab.py
+++ dl_1/lab.py
@@ -1,32 +1,42 @@
+import logging
 import os
 
+import dl_1.const as const
 from util.file.dataset_loader import prepare_dataset, get_path_to_unpacked_dir
 from util.file.image_handler import show_image
 from util.runner import Runner
-
-FIRST_DATASET_URL = "https://commondatastorage.googleapis.com/books1000/notMNIST_large.tar.gz"
-FIRST_UNIQ_DATASET_PATH_NAME = "notMnist_large"
-
-SECOND_DATASET_URL = "https://commondatastorage.googleapis.com/books1000/notMNIST_small.tar.gz"
-SECOND_UNIQ_DATASET_PATH_NAME = "notMnist_small"
 
 
 class Lab1(Runner):
 
     def __init__(self):
-        prepare_dataset(FIRST_DATASET_URL, FIRST_UNIQ_DATASET_PATH_NAME)
-        prepare_dataset(SECOND_DATASET_URL, SECOND_UNIQ_DATASET_PATH_NAME)
-        self.learning_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        prepare_dataset(const.FIRST_DATASET_URL, const.FIRST_UNIQ_DATASET_PATH_NAME)
+        prepare_dataset(const.SECOND_DATASET_URL, const.SECOND_UNIQ_DATASET_PATH_NAME)
 
     def run(self):
-        for letter in self.learning_letters:
-            path_to_img_dir = get_path_to_unpacked_dir(SECOND_UNIQ_DATASET_PATH_NAME)\
+        images_count = {}
+        for letter in const.LEARNING_LETTERS:
+            path_to_img_dir = get_path_to_unpacked_dir(const.FIRST_UNIQ_DATASET_PATH_NAME)\
                           + "/"\
-                          + SECOND_UNIQ_DATASET_PATH_NAME\
+                          + const.FIRST_UNIQ_DATASET_PATH_NAME\
                           + "/"\
                           + letter
             files_in_dir = os.listdir(path_to_img_dir)
             path_to_img = path_to_img_dir + "/" + files_in_dir[0]
             # (1)
             show_image(path_to_img)
+            # save count of images in letter in map
+            images_count[letter] = len(files_in_dir)
+
+        #(2)
+        print(images_count)
+        base = images_count[const.LEARNING_LETTERS[0]]
+        for letter in const.LEARNING_LETTERS:
+            if images_count[letter] - base > const.CLASSES_DIFFERENCE_ERROR:
+                logging.error("Images have too much error!")
+            logging.info("Classes have normal error. [letter %s , elements %s]", letter, images_count[letter])
+
+        # (3)
+
+
 
