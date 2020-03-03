c1b0f4292ede906783342cadd2b29368437bce8f
Maxim Petrusevich <maxim.petrusevich@minsk.leveris.com>
2020-03-03 13:32:20 +0300

    Fixed divinding on datasets

--- dl_1/lab.py
+++ dl_1/lab.py
@@ -1,10 +1,12 @@
 import logging
 import os
+import random
 
 import dl_1.const as const
 from util.file.dataset_loader import prepare_dataset, get_path_to_unpacked_dir
 from util.file.image_handler import show_image, calc_file_hash
 from util.runner import Runner
+import numpy as np
 
 
 class Lab1(Runner):
@@ -41,9 +43,9 @@ class Lab1(Runner):
         # (4)
         logging.info("Start deleting duplicates...")
         for letter in const.LEARNING_LETTERS:
-            path_to_img_dir = get_path_to_unpacked_dir(const.FIRST_UNIQ_DATASET_PATH_NAME)\
+            path_to_img_dir = get_path_to_unpacked_dir(const.SECOND_UNIQ_DATASET_PATH_NAME)\
                           + "/"\
-                          + const.FIRST_UNIQ_DATASET_PATH_NAME\
+                          + const.SECOND_UNIQ_DATASET_PATH_NAME\
                           + "/"\
                           + letter
             files_in_dir = os.listdir(path_to_img_dir)
@@ -55,4 +57,29 @@ class Lab1(Runner):
                     duplicate_images[hash] = file
 
         logging.info("Was found %d duplicate images, total count of unique images = %d", len(duplicate_images), len(uniq_images))
+
+        # (3)
+        # shuffle keys to get random images sets at every run
+        shuffled_images_keys = uniq_images.keys()
+
+        training_set = []
+        validation_set = []
+        test_set = []
+        count = 0
+
+        training_set_percentage = const.TRAIN_SET_PERCENTS
+        validation_set_percentage = const.TRAIN_SET_PERCENTS + const.VALIDATION_SET_PERCENTS
+        test_set_percentage = validation_set_percentage + const.TEST_SET_PERCENTS
+
+        for key in shuffled_images_keys:
+            current_percentage = (count / len(shuffled_images_keys))
+            print(current_percentage)
+            if current_percentage < training_set_percentage:
+                training_set.append(uniq_images[key])
+            if training_set_percentage < current_percentage < validation_set_percentage:
+                validation_set.append(uniq_images[key])
+            if validation_set_percentage < current_percentage < test_set_percentage:
+                test_set.append(uniq_images[key])
+            count += 1
+
 
